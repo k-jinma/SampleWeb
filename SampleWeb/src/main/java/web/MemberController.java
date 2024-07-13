ad9c2f422503be691d03f6d17a99db50385b59ae
k-jinma <jinmakazuhiro@outlook.com>
2024-07-13 13:38:54 +0900

    会員登録画面のバリデーションチェックを追加

--- SampleWeb/src/main/java/web/MemberController.java
+++ SampleWeb/src/main/java/web/MemberController.java
@@ -1,6 +1,8 @@
 package web;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import dao.MemberDAO;
 import jakarta.servlet.RequestDispatcher;
@@ -8,6 +10,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class MemberController
@@ -29,6 +32,8 @@ public class MemberController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
 		
 		String path = request.getParameter("path");
 		
@@ -57,7 +62,8 @@ public class MemberController extends HttpServlet {
 						
 					}else {
 						//みつからなかった＝登録ページへ
-						request.setAttribute("email", email);
+						//request.setAttribute("email", email);
+						session.setAttribute("email", email);
 						
 						RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/newmember-input-view.jsp");
 						dispatcher.forward(request, response);
@@ -70,6 +76,7 @@ public class MemberController extends HttpServlet {
 			case "newmember-input":
                 
 				// 入力パラメータを取得
+				email = request.getParameter("mailaddress");
 				String memberId = request.getParameter("memberId");
 				String memberName  = request.getParameter("memberName");
 				String gender = request.getParameter("gender");
@@ -78,8 +85,45 @@ public class MemberController extends HttpServlet {
 				String password = request.getParameter("password");
 				String passwordChk = request.getParameter("passwordChk");
 				
+				request.setAttribute("memberId", memberId);
+				request.setAttribute("memberName", memberName);
+				request.setAttribute("gender", gender);
+				request.setAttribute("address", address);
+				request.setAttribute("phone", phone);
+				
+				
 				// 入力チェック
-
+				List<String> messages = new ArrayList<>();
+				if( memberName.isBlank() ) {
+					messages.add("名前は必須項目です。");
+				}
+				if( address.isBlank() ) {
+					messages.add("住所は必須入力です。");
+				}
+				if( phone.isBlank() ) {
+					messages.add("電話番号は必須入力です。");
+				}
+				if( password.isBlank() ) {
+					messages.add("パスワードは必須入力です。");
+				}
+				if( passwordChk.isBlank() ) {
+					messages.add("パスワード（確認）は必須入力です。");
+				}
+				if( !password.isBlank() && !passwordChk.isBlank() ) {
+					if( !password.equals(passwordChk) ) {
+						messages.add("パスワードが一致しません。");
+					}
+				}
+				
+				if( messages.size() > 0 ) {
+					//request.setAttribute("messages", messages);
+					//request.setAttribute("email", email);
+					session.setAttribute("messages", messages);
+					
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/newmember-input-view.jsp");
+					dispatcher.forward(request, response);
+				}
+				
 				MemberDAO mDao = new MemberDAO();
 				//mDao.insertMember(memberId, memberName,gender,address,phone,password);
 				
@@ -89,5 +133,6 @@ public class MemberController extends HttpServlet {
 		
 		
 	}
+
 
 }
--- SampleWeb/src/main/webapp/jsp/newmember-input-view.jsp
+++ SampleWeb/src/main/webapp/jsp/newmember-input-view.jsp
@@ -1,37 +1,58 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>会員登録画面</title>
+
+<style>
+	.error_message {
+		color: red;
+	}
+</style>
+
 </head>
 <body>
+
+	<h1>会員登録</h1>
+	<c:if test="${not empty messages}">
+		<c:forEach var="message" items="${sessionScope.messages}">
+			<p class="error_message">${message}</p>
+		</c:forEach>
+	</c:if>
 	<form action="/SampleWeb/member?path=newmember-input" method="post">
 		<table>
 			<tr>
 				<td>会員ID</td>
-				<td><input type="text" name="email" size="30" value=${email} disabled></td>
+				<td><input type="text" name="email" size="30" value=${sessionScope.email} disabled></td>
 				<input type="hidden"name="memberId" value=${email}>
 			</tr>
 			<tr>
 				<td>名前</td>
-				<td><input type="text" name="memberName"></td>
+				<td><input type="text" name="memberName" value=${memberName}></td>
 			</tr>
 			<tr>
 				<td>性別</td>
 				<td>
-					<label><input type="radio" name="gender" value="1">男</label>
-					<label><input type="radio" name="gender" value="2">女</label>
+					<c:if test="${gender eq '1'}">
+						<label><input type="radio" name="gender" value="1" checked>男</label>
+						<label><input type="radio" name="gender" value="2">女</label>
+					</c:if>
+					<c:if test="${gender eq '2'}">
+						<label><input type="radio" name="gender" value="1">男</label>
+						<label><input type="radio" name="gender" value="2" checked>女</label>
+					</c:if>
 				</td>
 			</tr>
 			<tr>
 				<td>住所</td>
-				<td><input type="text" name="address" size="50"></td>
+				<td><input type="text" name="address" size="50" value=${address}></td>
 			</tr>
 			<tr>
 				<td>電話番号</td>
-				<td><input type="text" name="phone"></td>
+				<td><input type="text" name="phone" value=${phone}></td>
 			</tr>
 			<tr>
 				<td>パスワード</td>
